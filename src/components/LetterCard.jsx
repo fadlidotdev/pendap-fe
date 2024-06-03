88f70a972f71780110b7e37ff566db3af308e6d4
fadlihdytullah.dev <fadlihdytullah.dev@gmail.com>
2024-06-03 03:28:00 +0000

    add title letter card

--- src/components/LetterCard.jsx
+++ src/components/LetterCard.jsx
@@ -2,7 +2,7 @@
 export default function LetterCard({ data, onClick }) {
   return (
     <button
-      className="flex flex-col items-start gap-2 rounded-lg border p-3 text-left transition-all hover:bg-gray-100 bg-white shadow w-full relative"
+      className="flex flex-col items-start rounded-lg border p-3 text-left transition-all hover:bg-gray-100 bg-white shadow w-full relative"
       onClick={onClick}
     >
       <div className="flex justify-between w-full gap-2 items-center">
@@ -20,6 +20,10 @@ export default function LetterCard({ data, onClick }) {
         </div>
 
         <div className="text-xs text-gray-400">5 jam yg lalu</div>
+      </div>
+
+      <div className="text-sm lg:text-base">
+        Pengaduan Masyarakat Hukum Perdata
       </div>
 
       <div className="line-clamp-2 text-sm text-gray-400 text-left">
